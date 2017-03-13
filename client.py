c4691c43ff9fa3f11675134254f7e4bafeea23df
Jaume Giralt Barbé <jaume.giralt.2012@gmail.com>
2017-03-13 16:15:37 +0100

    Init Files

---
+++ client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+'''
+Client Web per Weather Underground
+
+@author: Jaume Giralt Barbé - jaume.giralt.2012@gmail.com
+'''
